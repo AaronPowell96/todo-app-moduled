60927e8ca23f8a6c2ee85b62626902767cb54571
Aaron <apowell829@gmail.com>
2019-01-27 12:47:17 +0000

    destructured getFilters

--- src/views.js
+++ src/views.js
@@ -4,12 +4,12 @@ import { getFilters } from "./filters"
 
 const renderTodos = () => {
     const todoEl = document.querySelector("#todoItems");
-    const filters = getFilters()
+    const { searchText, hideCompleted } = getFilters()
     const filteredTodos = getTodos().filter(todo => {
         const searchTextMatch = todo.text
             .toLowerCase()
-            .includes(filters.searchText.toLowerCase());
-        const hideCompletedMatch = !filters.hideCompleted || !todo.completed;
+            .includes(searchText.toLowerCase());
+        const hideCompletedMatch = !hideCompleted || !todo.completed;
 
         return searchTextMatch && hideCompletedMatch;
     });
