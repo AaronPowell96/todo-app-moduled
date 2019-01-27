2a1d3bd5345418483ba5de625d2c8c681eecec21
Aaron <Aaronpowell829@gmail.com>
2019-01-27 00:50:59 +0000

    Finalised index js for production

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
-import { getTodos, createTodo, removeTodo, toggleTodo } from "./todos"
+import { renderTodos, generateSummaryDom, generateTodoDOM } from "./views"
+import { setFilters } from "./filters"
+import { createTodo } from "./todos"
+renderTodos();
+
+document.querySelector("#searchTodos").addEventListener("input", e => {
+    setFilters({
+        searchText: e.target.value
+    })
+    renderTodos();
+});
+
+document.querySelector("#todoForm").addEventListener("submit", e => {
+    e.preventDefault();
+    let text = e.target.elements.todoItem.value.trim();
+    if (text.length > 0) {
+        createTodo(text);
+        renderTodos();
+        e.target.elements.todoItem.value = "";
+    }
+});
+
+document.querySelector("#hideCompleted").addEventListener("change", e => {
+    setFilters({
+        hideCompleted: e.target.checked
+    })
+    renderTodos();
+});
